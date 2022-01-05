4c667a432cdc2cc7b29e0e251a01d65ed3161ad6
nimrod <s.nimrod@campus.technion.ac.il>
2022-01-05 19:25:38 +0200

    finished Q2

--- Pokemons_App/views.py
+++ Pokemons_App/views.py
@@ -30,6 +30,16 @@ def query_results(request):
         """)
         sql_res1 = dictfetchall(cursor)
 
+        cursor.execute("""
+        select type, name from Pokemons P
+        except
+        select P1.type, P1.name
+        from Pokemons P1 join Pokemons P2 on P1.Type=P2.Type
+        WHERE ((p2.HP >= p1.HP or p2.Attack >= p1.Attack or p2.Defense >= p1.Defense) and p2.Name != p1.Name)
+        order by Type
+        """)
+        sql_res2 = dictfetchall(cursor)
+
         sql_res3 = {}
         if request.method == 'POST' and request.POST:
             threshold = request.POST["X"]
@@ -45,5 +55,19 @@ def query_results(request):
                                     HAVING MAX(p1.Attack) <= {threshold});
                                     """)
             sql_res3 = dictfetchall(cursor)
+
+        cursor.execute("""
+        select top 1 AvgInstbByType.Type, AvgInstbByType.Instability from (
+            select type, round(avg(sub),2) as instability from (
+                select type, cast(abs(Defense - Attack) as float) as sub from Pokemons
+                ) as InstbByPokemon
+                group by type)
+            as AvgInstbByType
+        order by instability desc
+        """)
+        sql_res4 = dictfetchall(cursor)
+
         return render(request, 'query_results.html', {'sql_res1': sql_res1,
-                                                      'sql_res3': sql_res3})
+                                                      'sql_res2': sql_res2,
+                                                      'sql_res3': sql_res3,
+                                                      'sql_res4': sql_res4})
